9377634fffc8453d9e6f13e8a374d2e0d281dd33
Thedath Oudarya <thedath.oudarya@gmail.com>
2023-01-20 14:21:36 +0530

    Initial commit

---
+++ lambda/runner.ts
@@ -0,0 +1,139 @@
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { AssumeRoleCommand, STSClient } from "@aws-sdk/client-sts";
+
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  if (!process.env.TABLE_NAME) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "Table name not found",
+      }),
+    };
+  }
+  const tableName = process.env.TABLE_NAME;
+
+  if (!process.env.TABLE_ARN) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "Table name not found",
+      }),
+    };
+  }
+  const tableArn = process.env.TABLE_ARN;
+
+  if (!process.env.ASSUMED_BY_ROLE_ARN) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "Access role not found",
+      }),
+    };
+  }
+  const assumedByRoleARN = process.env.ASSUMED_BY_ROLE_ARN;
+
+  if (!process.env.TEMP_SESSION_ROLE_ARN) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "Access role not found",
+      }),
+    };
+  }
+  const tempSessionRoleARN = process.env.TEMP_SESSION_ROLE_ARN;
+
+  const tenantId = event.queryStringParameters?.["tenantId"];
+  if (!tenantId) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "Tenant ID need to passed",
+      }),
+    };
+  }
+
+  // {
+  //   "Effect": "Allow",
+  //   "Principal": {
+  //     "AWS": "${assumedByRoleARN}"
+  //   },
+  //   "Action": "sts:AssumeRole"
+  // },
+
+  const policy = {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:Query"],
+        Resource: [tableArn],
+        Condition: {
+          "ForAllValues:StringLike": {
+            "dynamodb:LeadingKeys": ["beta"],
+          },
+        },
+      },
+    ],
+  };
+
+  const sts = new STSClient({});
+  const session = await sts.send(
+    new AssumeRoleCommand({
+      RoleArn: tempSessionRoleARN,
+      RoleSessionName: "TempSessionName",
+      DurationSeconds: 900,
+      // Tags: [
+      //   {
+      //     Key: "TenantID",
+      //     Value: "alpha",
+      //   },
+      // ],
+      Policy: JSON.stringify(policy),
+    })
+  );
+
+  // Configure the AWS SDK
+  const dynamoDb = new DynamoDBClient({
+    credentials: {
+      accessKeyId: session.Credentials?.AccessKeyId!,
+      secretAccessKey: session.Credentials?.SecretAccessKey!,
+      sessionToken: session.Credentials?.SessionToken,
+    },
+  });
+
+  try {
+    const result = await dynamoDb.send(
+      new QueryCommand({
+        TableName: tableName,
+        KeyConditionExpression: "#TIDkey = :TIDvalue",
+        ExpressionAttributeNames: {
+          "#TIDkey": "TenantID",
+        },
+        ExpressionAttributeValues: {
+          ":TIDvalue": {
+            S: tenantId,
+          },
+        },
+      })
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ...result.Items,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
--- lib/aws-tenant-isolation-stack.ts
+++ lib/aws-tenant-isolation-stack.ts
@@ -1,16 +1,169 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+  Stack,
+  StackProps,
+  aws_dynamodb as dynamodb,
+  aws_lambda as lambda,
+  aws_apigateway as apiGateway,
+  aws_iam as iam,
+  CfnOutput,
+} from "aws-cdk-lib";
+import { AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsTenantIsolationStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface AwsTenantIsolationStackProps extends StackProps {
+  account: string;
+}
+
+export class AwsTenantIsolationStack extends Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: AwsTenantIsolationStackProps
+  ) {
     super(scope, id, props);
+    props;
 
-    // The code that defines your stack goes here
+    // Define the IAM role
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsTenantIsolationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    const testTable = new dynamodb.Table(this, `${this.stackName}testTable`, {
+      tableName: `${this.stackName}testTable`,
+      partitionKey: { name: "TenantID", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "Timestamp", type: dynamodb.AttributeType.STRING },
+    });
+
+    // const dynamodbReadByTenantIDPolicy = new iam.PolicyStatement({
+    //   effect: iam.Effect.ALLOW,
+    //   actions: [
+    //     "dynamodb:BatchGetItem",
+    //     "dynamodb:BatchWriteItem",
+    //     "dynamodb:DeleteItem",
+    //     "dynamodb:GetItem",
+    //     "dynamodb:PutItem",
+    //     "dynamodb:Query",
+    //     "dynamodb:UpdateItem",
+    //   ],
+    //   resources: [testTable.tableArn],
+    //   conditions: {
+    //     "ForAllValues:StringEquals": {
+    //       "dynamodb:LeadingKeys": ["${aws:PrincipalTag/TenantID}"],
+    //     },
+    //   },
+
     // });
+    // tenantDataAccessGrantRole.addToPolicy(dynamodbReadByTenantIDPolicy);
+
+    const runner = new lambda.Function(this, `${this.stackName}RunnerLambda`, {
+      functionName: `${this.stackName}RunnerLambda`,
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "runner.handler",
+      code: lambda.Code.fromAsset("lambda"),
+      environment: {
+        TABLE_NAME: testTable.tableName,
+        TABLE_ARN: testTable.tableArn,
+      },
+    });
+    runner.addEnvironment("ASSUMED_BY_ROLE_ARN", runner.role?.roleArn!);
+
+    const tenantDataAccessGrantRole = new iam.Role(
+      this,
+      `${this.stackName}TenantDataAccessGrantRole`,
+      {
+        roleName: `${this.stackName}TenantDataAccessGrantRole`,
+        assumedBy: new iam.ArnPrincipal(runner.role?.roleArn!),
+      }
+    );
+    runner.addEnvironment(
+      "TEMP_SESSION_ROLE_ARN",
+      tenantDataAccessGrantRole.roleArn
+    );
+    tenantDataAccessGrantRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "dynamodb:BatchGetItem",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem",
+        ],
+        resources: [testTable.tableArn],
+      })
+    );
+
+    runner.role?.attachInlinePolicy(
+      new iam.Policy(this, "runnerInlinePolicy", {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["sts:AssumeRole"],
+            resources: [tenantDataAccessGrantRole.roleArn],
+          }),
+        ],
+      })
+    );
+
+    // runner.role?.grantAssumeRole(runner.role);
+
+    // runner.role?.addToPrincipalPolicy(
+    //   new iam.PolicyStatement({
+    //     effect: iam.Effect.ALLOW,
+    //     actions: ["sts:TagSession"],
+    //     conditions: {
+    //       StringLike: {
+    //         "aws:RequestTag/TenantID": "*",
+    //       },
+    //     },
+    //   })
+    // );
+
+    // const tenantIsolationInitRole = new iam.Role(
+    //   this,
+    //   `${this.stackName}TenantIsolationInitRole`,
+    //   {
+    //     roleName: `${this.stackName}TenantIsolationInitRole`,
+    //     assumedBy: new iam.ArnPrincipal(runner.role?.roleArn!),
+    //   }
+    // );
+
+    // Define the policy statements
+    // const assumeRolePolicyStatement = new iam.PolicyStatement({
+    //   effect: iam.Effect.ALLOW,
+    //   actions: ["sts:AssumeRole"],
+    //   // principals: [new iam.ArnPrincipal("lambda.amazonaws.com")],
+    // });
+    // const tagSessionPolicyStatement = new iam.PolicyStatement({
+    //   effect: iam.Effect.ALLOW,
+    //   actions: ["sts:TagSession"],
+    //   // principals: [new iam.ServicePrincipal("lambda.amazonaws.com")],
+    //   conditions: {
+    //     StringLike: {
+    //       "aws:RequestTag/TenantID": "*",
+    //     },
+    //   },
+    // });
+    // tenantIsolationInitRole.addToPolicy(assumeRolePolicyStatement);
+    // tenantIsolationInitRole.addToPolicy(tagSessionPolicyStatement);
+
+    new CfnOutput(this, "RoleOutput", {
+      exportName: "RoleOutput",
+      value: runner.role?.roleArn || "No name",
+    });
+
+    new CfnOutput(this, "RolePolicy", {
+      exportName: "RolePolicy",
+      value:
+        JSON.stringify(runner.role?.policyFragment.principalJson) || "No name",
+    });
+
+    const api = new apiGateway.RestApi(this, `${this.stackName}API`, {
+      restApiName: `${this.stackName}API`,
+    });
+    api.root.addResource("test");
+    api.root
+      .getResource("test")
+      ?.addMethod("GET", new apiGateway.LambdaIntegration(runner));
   }
 }
