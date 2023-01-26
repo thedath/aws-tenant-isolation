1c2c4c5217eab2018135c28eb6e6c09f3eee7028
Thedath Oudarya <thedath.oudarya@gmail.com>
2023-01-26 21:20:56 +0530

    Keep code ralated to lambda backed custom resource
    Signed-off-by: Thedath Oudarya <thedath.oudarya@gmail.com>

--- lib/aws-tenant-isolation-stack.ts
+++ lib/aws-tenant-isolation-stack.ts
@@ -1,17 +1,15 @@
 import {
-  aws_apigateway as apiGateway,
   aws_dynamodb as dynamodb,
   aws_iam as iam,
   aws_lambda as lambda,
   aws_s3 as s3,
-  custom_resources as cr,
   CustomResource,
+  custom_resources as cr,
   RemovalPolicy,
   Stack,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import getConstants from "../lambda/constants";
-import RoleAssumingLambda from "./RoleAssumingLambda";
 export class AwsTenantIsolationStack extends Stack {
   readonly constants;
 
@@ -109,71 +107,5 @@ export class AwsTenantIsolationStack extends Stack {
     );
     customResource.node.addDependency(dynamodbTable);
     customResource.node.addDependency(s3Bucket);
-
-    const readDynamoWithLeadingKeysPolicy = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ["dynamodb:Query"],
-      resources: [dynamodbTable.tableArn],
-      conditions: {
-        "ForAllValues:StringLike": {
-          "dynamodb:LeadingKeys": [
-            `\${aws:PrincipalTag/${this.constants.SESSION_TAG_KEY}}`,
-          ],
-        },
-      },
-    });
-
-    const dynamodbReadLambda = new RoleAssumingLambda(
-      this,
-      this.constants.DYNAMODB_READ_LAMBDA_NAME,
-      {
-        functionName: this.constants.DYNAMODB_READ_LAMBDA_NAME,
-        runtime: lambda.Runtime.NODEJS_14_X,
-        handler: "dynamodbReader.handler",
-        code: lambda.Code.fromAsset("lambda"),
-        assumedRolePolicyStatements: [readDynamoWithLeadingKeysPolicy],
-        assumedRoleArnEnvKey: this.constants.ASSUMED_ROLE_ARN_ENV_KEY_1,
-        sessionTag: this.constants.TABLE_PARTITION_KEY,
-      }
-    );
-
-    const getBucketObjectWithPrefix = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ["s3:ListBucket"],
-      resources: [s3Bucket.bucketArn],
-      conditions: {
-        StringEquals: {
-          "s3:prefix": `\${aws:PrincipalTag/${this.constants.SESSION_TAG_KEY}}`,
-        },
-      },
-    });
-
-    const s3BucketReadLambda = new RoleAssumingLambda(
-      this,
-      this.constants.S3_BUCKET_READ_LAMBDA_NAME,
-      {
-        functionName: this.constants.S3_BUCKET_READ_LAMBDA_NAME,
-        runtime: lambda.Runtime.NODEJS_14_X,
-        handler: "s3BucketReader.handler",
-        code: lambda.Code.fromAsset("lambda"),
-        assumedRolePolicyStatements: [getBucketObjectWithPrefix],
-        assumedRoleArnEnvKey: this.constants.ASSUMED_ROLE_ARN_ENV_KEY_2,
-        sessionTag: this.constants.S3_BUCKET_NAME,
-      }
-    );
-
-    const api = new apiGateway.RestApi(this, `${this.stackName}API`, {
-      restApiName: `${this.stackName}API`,
-    });
-
-    api.root.addResource("readDynamodb");
-    api.root
-      .getResource("readDynamodb")
-      ?.addMethod("GET", new apiGateway.LambdaIntegration(dynamodbReadLambda));
-
-    api.root.addResource("readS3Bucket");
-    api.root
-      .getResource("readS3Bucket")
-      ?.addMethod("GET", new apiGateway.LambdaIntegration(s3BucketReadLambda));
   }
 }
